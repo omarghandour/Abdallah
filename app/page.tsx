92af3ebfc1ade63a5bba03a815cdf77c613d3875
omarghandour <109479370+omarghandour@users.noreply.github.com>
2023-11-02 19:07:40 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Info from "./Components/Info";
 import Mainee from "./Components/Mainee";
 
 export default function Home() {
   return (
 <div className="pg">
-       <Mainee />
+       {/* <Mainee /> */}
+       <Info />
 </div>
   )
 }
